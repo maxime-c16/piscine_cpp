5c7a9758dee25a780623364ed0219c9e8942a110
maxime cauchy <mcauchy@student.42.fr>
2022-12-27 11:54:23 +0000

    added <limits>

--- cpp00/ex01/header.hpp
+++ cpp00/ex01/header.hpp
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 14:33:17 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/11/25 18:25:55 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/12/27 10:45:00 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <iostream>
 # include <string>
 # include <iomanip>
+# include <limits>
 # include "PhoneBook.hpp"
 # include "Contact.hpp"
 
