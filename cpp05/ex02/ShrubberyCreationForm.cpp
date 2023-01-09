ed2f2580c3da26c6415ff3de1fc0b2058d2de862
maxime cauchy <mcauchy@student.42.fr>
2023-01-09 10:16:33 +0000

    ciao once again

--- cpp05/ex02/ShrubberyCreationForm.cpp
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 12:52:45 by mcauchy           #+#    #+#             */
-/*   Updated: 2023/01/09 09:06:38 by mcauchy          ###   ########.fr       */
+/*   Updated: 2023/01/09 10:15:16 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,23 +62,17 @@ void ShrubberyCreationForm::execute(const Bureaucrat& executor) const
 std::string ShrubberyCreationForm::generateRandomTree( void ) const
 {
 	std::string tree;
-	// Seed the random number generator with the current time
 	srand(time(0));
-	// Generate a random number between 3 and 7 to determine the height of the tree
 	int treeHeight = rand() % 5 + 3;
-	// Generate the tree using a for loop
 	for (int i = 0; i < treeHeight; i++) {
-		// Generate the spaces to align the tree
 		for (int j = 0; j < treeHeight - i - 1; j++) {
 			tree += " ";
 		}
-		// Generate the tree branches
 		for (int j = 0; j < 2 * i + 1; j++) {
 			tree += "*";
 		}
 		tree += "\n";
 	}
-	// Generate the tree trunk
 	for (int i = 0; i < treeHeight / 2 + 1; i++) {
 		for (int j = 0; j < treeHeight - 1; j++) {
 			tree += " ";
@@ -86,18 +80,14 @@ std::string ShrubberyCreationForm::generateRandomTree( void ) const
 		tree += "|\n";
 	}
 
-	// Generate some fallen leaves
 	for (int i = 0; i < treeHeight; i++)
 	{
-		// Generate a random number of fallen leaves
 		int numLeaves = rand() % 5 + 1;
 
-		// Generate the spaces to align the fallen leaves
 		for (int j = 0; j < treeHeight - i - 1; j++) {
 			tree += " ";
 		}
 
-		// Generate the fallen leaves
 		for (int j = 0; j < numLeaves; j++)
 		{
 			tree += "X";
