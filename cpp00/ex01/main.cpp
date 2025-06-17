14d5ff32cb025d1071420e2261b5c7e95c217466
CAUCHY maxime <macauchy@student.42.fr>
2025-06-17 12:22:59 +0200

    fix: update author information and improve input handling in main.cpp

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: macauchy <macauchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 14:32:35 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/12/29 10:48:37 by mcauchy          ###   ########.fr       */
+/*   Updated: 2025/06/17 12:22:40 by macauchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int	main(void)
 	{
 		std::string	command;
 		std::cout << "Enter a command: ";
-		std::cin >> command;
+		if (!(std::cin >> command))
+			break;
 		if (command == "ADD")
 		{
 			std::cout << "Enter first name: ";
@@ -52,13 +53,16 @@ int	main(void)
 		else if (command == "SEARCH")
 		{
 			phonebook.displayAllContacts();
-			std::cout << "Enter the index of the contact you want to display: ";
 			while (!(std::cin >> index) || index > 8 || index < 1)
 			{
+				if (std::cin.eof())
+					break;
 				std::cin.clear();
 				std::cin.ignore(INT_MAX, '\n');
 				std::cout << "Invalid index, please enter a valid index: ";
 			}
+			if (!std::cin.eof())
+				phonebook.displayContact(index - 1);
 			phonebook.displayContact(index - 1);
 		}
 		else if (command == "EXIT")
